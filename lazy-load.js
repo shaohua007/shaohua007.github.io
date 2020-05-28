fbb90a0310f531b62129b45fde6047ce034b72a6
shaohua007 <136821423@qq.com>
2020-05-28 14:19:34 +0800

    Update and rename lazy.html to lazy-load.js

---
+++ lazy-load.js
@@ -0,0 +1,48 @@
+(function () {
+    let _lazyImg = function (option) {
+        const options = {
+            className: 'lazy', //  需要懒加载的图片的类名
+            delay: 100          // 设置防抖延时
+        }
+        Object.assign(options, option)
+        let imgEleArr = document.getElementsByClassName(options.className)
+        const eleArr = Array.prototype.slice.call(imgEleArr)
+        start()
+        loadCtr()
+        
+        function start() {
+            eleArr.forEach(function (ele) {
+                if (isLoad(ele) && !hasLaded(ele)) {
+                    ele.src = ele.getAttribute('pic')
+                }
+            })
+        }
+        function loadCtr() {
+            window.onscroll = debounce(start, options.delay)
+        }
+        function isLoad(curImg) {
+            let clientH = document.body.clientHeight
+            let imgTop = curImg.offsetTop
+            let imgH = curImg.offsetHeight
+            let winTop = window.pageYOffset
+            //  确保图片在窗口可视范围内
+            return winTop + clientH > imgTop && imgTop > winTop - imgH
+        }
+        function hasLaded(curImg) {
+            return curImg.src === curImg.getAttribute('pic')
+        }
+        function debounce(func, delay) {
+            let timer = null;
+            return function () {
+                if (timer) {
+                    clearTimeout(timer)
+                }
+                timer = setTimeout(function () {
+                    timer = null
+                    func()
+                }, delay)
+            }
+        }
+    }
+    window.lazyImg = _lazyImg
+})()
